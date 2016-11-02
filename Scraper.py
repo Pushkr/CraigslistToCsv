9523ad46a6e79a513013b262b47f50792deade56
PUSHKAR GUJAR <pushkarvgujar@gmail.com>
2016-11-01 22:10:10 -0400

    Update and rename Scrapper.py to Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -7,7 +7,8 @@ import requests
 from bs4 import BeautifulSoup
 from requests.exceptions import ChunkedEncodingError
 from urllib.parse import urlparse
-
+from queue import Queue
+import threading
 
 class GetCraiglistData:
     """ Use this class to search,save and/or print craiglist data"""
@@ -15,6 +16,14 @@ class GetCraiglistData:
     url = "https://newyork.craigslist.org/"
     s_page = "&s="
     search_url = "search/sss?&query="
+    Qurl = Queue()
+    pageWorkerQueue = Queue()
+    rowWorkerQueue = Queue()
+    fetchList_lock = threading.Lock()
+    appendList_lock = threading.Lock()
+    result_pages = []
+    items = []
+    targetName = ''
 
     def __init__(self, search_term):
         """ Enter item to search on craigslist.
@@ -31,7 +40,7 @@ class GetCraiglistData:
     @property
     def geturl(self):
         """
-        :return: craiglist site url used for searching
+        :rtype: craiglist site url used for searching
         """
         return self.url
 
@@ -47,9 +56,118 @@ class GetCraiglistData:
             print("\nCannot set requested URL, Site response code : %s" % scode)
             print("\nreverting back to default url.. %s" % url)
 
+    def pageFetcher(self,worker):
+        temp_url = None
+        with self.fetchList_lock:
+            if self.Qurl.empty() is False:
+                temp_url = self.Qurl.get()
+
+        if temp_url is not None:
+            try:
+                webdata_full = requests.get(temp_url)
+                soup = BeautifulSoup(webdata_full.text, "html.parser")
+                result1 = soup.find("div", {"class": "content"})
+                temp_rows = result1.find_all("p", {"class": "row"})
+            except (ConnectionRefusedError, ConnectionResetError, ConnectionAbortedError) as err:
+                print("\nError connecting site : {0} \n".format(err))
+                return
+            except ChunkedEncodingError:
+                print("\nSite response delayed, skipping retrieval..: {0}".format(sys.exc_info()[0]))
+            finally:
+                with self.appendList_lock:
+                    #print(threading.current_thread().name, self.Qurl.qsize(), temp_url)
+                    if len(temp_rows) > 0:
+                        self.result_pages.append(temp_rows)
+        else:
+            pass
+
+    def rowFetcher(self, worker):
+        row = None
+        with self.fetchList_lock:
+            if self.Qurl.empty() is False:
+                row = self.Qurl.get()
+
+        id_url = row.find("a", {"class": "hdrlnk"})
+        lurl = (id_url.get("href"))
+        posting = urlparse(lurl)
+        if posting.netloc == '':
+            post_url = urlparse(self.url).scheme + "://" + urlparse(self.url).netloc + posting.path
+        else:
+            post_url = urlparse(self.url).scheme + "://" + posting.netloc + posting.path
+
+        title = id_url.text if id_url is not None else "No Title"
+
+        span = row.find("span", {"class": "price"})
+        price = (span.text if span is not None else "Not Listed")
+
+        # Retrieve post details
+        try:
+            post_data = requests.get(post_url)
+            post_soup = BeautifulSoup(post_data.text, "html.parser")
+            pspan = post_soup.find("span", {"class": "postingtitletext"})
+            pbody = post_soup.find("section", {"id": "postingbody"})
+        except (ConnectionRefusedError, ConnectionResetError, ConnectionAbortedError) as err:
+            print("\nError connecting site : {0} \n".format(err))
+            return
+        except ChunkedEncodingError:
+            print("\nSite response delayed, skipping retrieval..: {0}".format(sys.exc_info()[0]))
+
+        location = "Not Listed"
+        try:
+            location = pspan.small.text if pspan is not None  else "Not Listed"
+        except AttributeError:
+            pass
+
+        body_text = pbody.text if pbody is not None else "Not Listed"
+
+        pbody = post_soup.find_all("p", {"class": "postinginfo"})
+
+        post_time, upd_time = ["N/A", "N/A"]
+        try:
+            if pbody[2].find("time", {"class": "timeago"}) is not None:
+                post_time = (pbody[2].find("time", {"class": "timeago"}))['datetime'].split("T")
+            if pbody[3].find("time", {"class": "timeago"}) is not None:
+                upd_time = (pbody[3].find("time", {"class": "timeago"}))['datetime'].split("T")
+        except:
+            pass
+
+        with self.appendList_lock:
+            self.items.append((title, post_url, price, location, post_time[0], post_time[1][:-5],
+                           upd_time[0], upd_time[1][:-5], body_text))
+
+    def pageThreader(self):
+        while True:
+            worker = self.pageWorkerQueue.get()
+            self.pageFetcher(worker)
+            self.pageWorkerQueue.task_done()
+
+
+    def start_pageThreads(self):
+        for x in range(4): # Four threads
+            t = threading.Thread(target=self.pageThreader)
+            t.daemon = True
+            t.start()
+
+        for worker in range(self.Qurl.qsize()):
+            self.pageWorkerQueue.put(worker)
+        self.pageWorkerQueue.join() # block until item in queue is processed
+
+    def rowThreader(self):
+        while True:
+            worker = self.rowWorkerQueue.get()
+            self.rowFetcher(worker)
+            self.rowWorkerQueue.task_done()
+
+    def start_rowThreads(self):
+        for x in range(4):  # Four threads
+            t = threading.Thread(target=self.rowThreader)
+            t.daemon = True
+            t.start()
+        for worker in range(self.Qurl.qsize()):
+            self.rowWorkerQueue.put(worker)
+        self.rowWorkerQueue.join() # block until item in queue is processed
+
     def __getTotalresults__(self):
-        # self.rangeFrom = 0
-        # self.rangeTo = 0
         self.totalCount = 0
         pages = {}
         try:
@@ -67,30 +185,27 @@ class GetCraiglistData:
             return
 
         if pages is not None and pages.text != 'no results':
-            # self.rangeFrom = int(pages.find("span", {"class": "rangeFrom"}).text)
-            # self.rangeTo = int(pages.find("span", {"class": "rangeTo"}).text)
-            self.totalCount = int(pages.find("span", {"class": "totalcount"}).text)
+           self.totalCount = int(pages.find("span", {"class": "totalcount"}).text)
         else:
-            # self.rangeFrom = 0
-            # self.rangeTo = 0
             self.totalCount = 0
 
     def printresults(self, limit=None):
         """print the search results on output console
         """
-        self.__retrievedata__()
+        self.__retrievedata__(limit)
         if len(self.items) == 0:
             print("No results")
         elif limit is None:
             for item in self.items:
                 print(item[0], "\t", item[1], "\t", item[2])
         elif 0 < limit <= len(self.items):
-            for i in range(0, limit + 1):
+            for i in range(0, limit):
                 print(self.items[i][0], "\t", self.items[i][1], "\t", self.items[i][2])
         elif limit > len(self.items):
             print("Warning : not enough search results. Printing available items.. ")
             for item in self.items:
                 print(item[0], "\t", item[1], "\t", item[2])
+        sys.exit(1)
 
     def saveresults(self):
         """ Save the search result in current_path/SearchResults.csv
@@ -109,91 +224,38 @@ class GetCraiglistData:
                 print("\nResult saved in SearchResults.csv\nGoodbye.")
             except IOError:
                 print("\nError saving data in file : %s" % sys.exc_info()[0])
+        #sys.exit(1)
 
-    def __retrievedata__(self):
+    def __retrievedata__(self,limit=None):
         self.__getTotalresults__()
         if self.totalCount == 0:
             return self.items
 
-        result_pages = []
-        # del soup, webdata
-        print("\nFound %s items" % self.totalCount)
+        if limit is None:
+            print("\nFound %s items" % self.totalCount)
+        else:
+            print("\nFound {} items, retrieving first {} ".format(self.totalCount,limit))
+            self.totalCount = limit
+
         print("\nFetching data...")
-        for i in range(0, self.totalCount // 100 + 2):
-            qualifiedurl = (self.url + self.search_url + self.search_term + self.s_page + str(i * 100))
-            # print("Fetching data from %s......" % qualifiedurl)
-            try:
-                webdata_full = requests.get(qualifiedurl)
-                # print("web response: ", webdata_full)
-                soup = BeautifulSoup(webdata_full.text, "html.parser")
-                result1 = soup.find("div", {"class": "content"})
-                temp_rows = result1.find_all("p", {"class": "row"})
-                # near_areas = (soup.find("li", {"class": "crumb area"})).find_all("option")
-            except (ConnectionRefusedError, ConnectionResetError, ConnectionAbortedError) as err:
-                print("\nError connecting site : {0} \n".format(err))
-                return
-            except ChunkedEncodingError:
-                print("\nSite response delayed, skipping retrieval..: {0}".format(sys.exc_info()[0]))
-            finally:
-                if len(temp_rows) > 0:
-                    result_pages.append(temp_rows)
 
-        # # print(near_areas)
-        # for area in near_areas:
-        #     print(area['value'], area.text)
+        # Flush queue
+        self.Qurl.queue.clear()
+        # Fill queue
+        for i in range(0, self.totalCount // 100):
+            self.Qurl.put((self.url + self.search_url + self.search_term + self.s_page + str(i * 100)))
 
-        for aPage in result_pages:
+        #print(self.Qurl.qsize())
+        self.start_pageThreads()
+
+        # Flush queue
+        self.Qurl.queue.clear()
+        # Fill queue
+        for aPage in self.result_pages:
             for row in aPage:
-                id_url = row.find("a", {"class": "hdrlnk"})
-                lurl = (id_url.get("href"))
-                posting = urlparse(lurl)
-                if posting.netloc == '':
-                    post_url = urlparse(self.url).scheme + "://" + urlparse(self.url).netloc + posting.path
-                else:
-                    post_url = urlparse(self.url).scheme + "://" + posting.netloc + posting.path
-
-                # print(post_url)
-                # title_find = id_url.find("span", {"id": "titletextonly"})
-                # title = title_find.text if title_find is not None else "Not Listed"
-
-                # Fix for issue #2
-                title = id_url.text if id_url is not None else "No Title"
-
-                span = row.find("span", {"class": "price"})
-                price = (span.text if span is not None else "Not Listed")
-
-                # Retrieve post details
-                try:
-                    post_data = requests.get(post_url)
-                    post_soup = BeautifulSoup(post_data.text, "html.parser")
-                except (ConnectionRefusedError, ConnectionResetError, ConnectionAbortedError) as err:
-                    print("\nError connecting site : {0} \n".format(err))
-                    return
-                except ChunkedEncodingError:
-                    print("\nSite response delayed, skipping retrieval..: {0}".format(sys.exc_info()[0]))
-
-                pspan = post_soup.find("span", {"class": "postingtitletext"})
-                pbody = post_soup.find("section", {"id": "postingbody"})
-
-                location = pspan.small.text if pspan.small is not None else "Not Listed"
-
-                body_text = pbody.text if pbody is not None else "Not Listed"
-
-                pbody = post_soup.find_all("p", {"class": "postinginfo"})
-
-                if pbody[2].find("time", {"class": "timeago"}) is not None:
-                    post_time = (pbody[2].find("time", {"class": "timeago"}))['datetime'].split("T")
-                else:
-                    post_time = ["N/A", "N/A"]
-
-                if pbody[3].find("time", {"class": "timeago"}) is not None:
-                    upd_time = (pbody[3].find("time", {"class": "timeago"}))['datetime'].split("T")
-                else:
-                    upd_time = ["N/A", "N/A"]
-
-                self.items.append((title, post_url, price, location, post_time[0], post_time[1][:-5],
-                                   upd_time[0], upd_time[1][:-5], body_text))
-
+                self.Qurl.put(row)
+        print(self.Qurl.qsize())
+        self.start_rowThreads()
         self.items = list(set(self.items))  # remove the duplicates.
         print("\nRetrieved %d items \n" % (len(self.items)))
 
@@ -243,16 +305,16 @@ class GetCraiglistSites:
 
     def getsuggestions(self, city):
         print("finding recommendations.. ")
-        city_found = 0
+        city_found = False
         i = 1
         city = city.lower()
         for keys, values in self.site_map.items():
             if city[:3] == keys[:3]:
-                city_found = 1
+                city_found = True
                 print("%d . City : %s URL : %s " % (i, keys, values))
                 i += 1
 
-        if city_found == 0:
+        if not city_found:
             print("sorry, no recommendations.")
             return 404
 
